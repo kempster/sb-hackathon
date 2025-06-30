f170a30611c073aa1bff7430e151abee547b56ac
Matt Kempster <matt@unified.co>
2025-06-30 12:56:03 +0100

    first

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import StoryblokProvider from "@/components/StoryblokProvider";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -18,12 +19,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <StoryblokProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </StoryblokProvider>
   );
 }
