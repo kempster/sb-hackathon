f22adcbf3391a37ea666f1a949a2768c53abfa7c
Marj <13993214+marjmartinezz@users.noreply.github.com>
2025-06-30 14:32:24 +0100

    fix classname

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,7 +4,7 @@ export default function Grid({ blok }){
 return (
 	<div className="grid grid-cols-3 gap-5 mb-10">
 		{blok.columns.map((nestedBlok) => (
-			<div class="aspect-square bg-gray-200"> 
+			<div className="aspect-square bg-gray-200"> 
 				<StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
 			</div>
 		))}
--- src/components/Teaser.jsx
+++ src/components/Teaser.jsx
@@ -1,7 +1,7 @@
 export default function Teaser({ blok }) {
 return (
 	<div className="mb-10">
-		<h2 class="font-black text-6xl">{blok.headline}</h2>
+		<h2 className="font-black text-6xl">{blok.headline}</h2>
 	</div>
 );
 }
